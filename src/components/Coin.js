d29407abb302c167c19b22662bcddbbf6ecab307
Reza Alaedini <parsa.alaei.pa@gmail.com>
2022-07-16 20:01:13 +0430

    All Done ! Late for Commit :)

---
+++ src/components/Coin.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+//Style
+import styles from "./Coin.module.css";
+
+const Coin = ({ name, symbol, image, price, marketCap, priceChange }) => {
+  return (
+    <div className={styles.container}>
+      <div
+        className={
+          priceChange > 0 ? styles.coinContainerDec : styles.coinContainerAsc
+        }
+      >
+        <img src={image} alt="coinImg" />
+        <span className={styles.coinName}>{name}</span>
+        <span>{symbol.toUpperCase()}</span>
+        <span>$ {price.toLocaleString()}</span>
+        <span>{priceChange.toFixed(2)} %</span>
+        <span>$ {marketCap.toLocaleString()}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,30 +3,57 @@ import React, { useEffect, useState } from "react";
 //API
 import { getCoins } from "../services/Api";
 
+//Style
+import styles from "./Landing.module.css";
+
 // Components
 import Loader from "./Loader";
+import Coin from "./Coin";
 
 const Landing = () => {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const fetchAPI = async () => {
       const data = await getCoins();
-      console.log(data);
+      // console.log(data);
       setCoins(data);
     };
     fetchAPI();
   }, []);
 
+  const searchHandler = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchedCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <>
-      <input type="text" placeholder="Search" />
+      <input
+        type="text"
+        placeholder="Search"
+        value={search}
+        onChange={searchHandler}
+        className={styles.search}
+      />
       <br />
 
       {coins.length ? (
         <div>
-          {coins.map((coin) => (
-            <p key={coin.id} style={{color: "white"}}>{coin.name}</p>
+          {searchedCoins.map((coin) => (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              symbol={coin.symbol}
+              image={coin.image}
+              price={coin.current_price}
+              marketCap={coin.market_cap}
+              priceChange={coin.price_change_percentage_24h}
+            />
           ))}
         </div>
       ) : (
