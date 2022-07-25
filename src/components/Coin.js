56bcf4ae39110b97169df145a8ee018296da4eaa
Reza Alaedini <parsa.alaei.pa@gmail.com>
2022-07-25 16:46:03 +0430

    edit Coin Component

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -11,11 +11,11 @@ const Coin = ({ name, symbol, image, price, marketCap, priceChange }) => {
           priceChange > 0 ? styles.coinContainerDec : styles.coinContainerAsc
         }
       >
-        <img src={image} alt="coinImg" />
+        <img src={image} alt="coinImg" className={styles.coinImg} />
         <span className={styles.coinName}>{name}</span>
         <span>{symbol.toUpperCase()}</span>
         <span>$ {price.toLocaleString()}</span>
-        <span>{priceChange.toFixed(2)} %</span>
+        <span>% {priceChange.toFixed(2)}</span>
         <span>$ {marketCap.toLocaleString()}</span>
       </div>
     </div>
